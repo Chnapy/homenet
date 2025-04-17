f5c92ab9488882e985eacb89b1420e9d65e2f898
Richard Haddad <richardhaddad@hotmail.fr>
2025-04-17 19:57:43 +0000

    fix auto-update with restart

--- agent/src/auto-update.go
+++ agent/src/auto-update.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"syscall"
 
 	"github.com/blang/semver"
 	"github.com/rhysd/go-github-selfupdate/selfupdate"
@@ -34,7 +38,32 @@ func AutoUpdate(releaseTag string) error {
 		fmt.Printf("Agent already up-to-date %s\n", latestRelease.Version.String())
 	} else {
 		fmt.Printf("Agent up-to-date from %s to %s\n", v.String(), latestRelease.Version.String())
+		restart()
+		os.Exit(0)
 	}
 
 	return nil
+}
+
+func restart() error {
+	exe, err := os.Executable()
+	if err != nil {
+		return err
+	}
+	args := os.Args
+
+	fmt.Println("App restarting...")
+
+	// Unix
+	if runtime.GOOS == "linux" {
+		env := os.Environ()
+		return syscall.Exec(exe, args, env)
+	}
+
+	// Windows or fallback
+	cmd := exec.Command(exe, args[1:]...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	return cmd.Start()
 }
