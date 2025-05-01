ad76e9a7d371e8847d2cd41bb1c180eb29cf0a4c
Richard Haddad <richardhaddad@hotmail.fr>
2025-05-01 01:03:54 +0000

    fix

--- frontend/src/components/navigation/sidebar-item-home.tsx
+++ frontend/src/components/navigation/sidebar-item-home.tsx
@@ -1,5 +1,6 @@
-import { ListItem, ListItemButton, ListItemIcon } from "@mui/material";
+import { Box, ListItem, ListItemButton, ListItemIcon } from "@mui/material";
 import React from "react";
+import hnLogoPath from "../../assets/icons/hn-logo.svg";
 import { useCurrentPage } from "./hooks/use-current-page";
 
 export const SidebarItemHome: React.FC = () => {
@@ -8,7 +9,9 @@ export const SidebarItemHome: React.FC = () => {
   return (
     <ListItem disablePadding>
       <ListItemButton href="#" selected={selected}>
-        <ListItemIcon sx={{ minWidth: 24, maxWidth: 24 }}>HN</ListItemIcon>
+        <ListItemIcon sx={{ minWidth: 24, maxWidth: 24 }}>
+          <Box component="img" src={hnLogoPath} sx={{ maxWidth: "100%" }} />
+        </ListItemIcon>
       </ListItemButton>
     </ListItem>
   );
