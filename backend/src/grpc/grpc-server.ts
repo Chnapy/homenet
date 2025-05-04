025bfe9963b193fde50282a532617e2828a29506
Richard Haddad <richardhaddad@hotmail.fr>
2025-05-04 13:02:07 +0000

    fix

--- backend/src/grpc/grpc-server.ts
+++ backend/src/grpc/grpc-server.ts
@@ -1,13 +1,15 @@
 import * as grpc from "@grpc/grpc-js";
 import * as protoLoader from "@grpc/proto-loader";
 import { ReflectionService } from "@grpc/reflection";
+import path from "node:path";
 import { AgentService } from "./generated/agent";
 import { gRPCRouter } from "./grpc-router";
 
 export const setupGRPCServer = () => {
   const gRPCServer = new grpc.Server();
 
-  const packageDefinition = protoLoader.loadSync("./protos/agent.proto");
+  const protoPath = path.resolve("./protos/agent.proto");
+  const packageDefinition = protoLoader.loadSync(protoPath);
   const reflection = new ReflectionService(packageDefinition);
 
   reflection.addToServer(gRPCServer);
